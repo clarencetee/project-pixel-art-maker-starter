723e12fa5977db65dbeeb1eeb1ddf992870cea1c
clarence tee <yoonsiangtee@gmail.com>
2020-04-23 15:49:41 +0100

    added initial makegrid code

--- designs.js
+++ designs.js
@@ -1,10 +1,23 @@
-// Select color input
-// Select size input
+// Let's define some variables for us to manipulate later
+const gridColour = document.querySelector('#colorPicker');
+const gridRow = document.querySelector('#inputHeight');
+const gridHeight = document.querySelector('#inputWidth');
+const pixelcanvas=document.querySelector('#pixelCanvas');
+
 
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
+    for (row=1; row<=gridRow; row++) {
+        let newrow = document.createElement ('tr');
+        for (column=1; column<=gridHeight; column++) {
+            let newcolumn = document.createElement('td');
+            let pixelgrid= newrow.appendChild(newcolumn);
+        }
+    }
+    let newtable = pixelcanvas.appendChild(pixelgrid);
 
+};
+    l
 // Your code goes here!
-
-}
+document.querySelector('#submission').addEventListener('click', makeGrid);
