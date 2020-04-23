56a212f20fa2e22d2365d0eb841d55a316c4ec5f
clarence tee <yoonsiangtee@gmail.com>
2020-04-23 22:37:53 +0100

    finished file

--- designs.js
+++ designs.js
@@ -1,30 +1,38 @@
 // Let's define some variables for us to manipulate later
-const gridColour = document.querySelector('#colorPicker').value;
-const gridRow = document.querySelector('#inputHeight').value;
-const gridColumn = document.querySelector('#inputWidth').value;
 const pixelcanvas=document.querySelector('#pixelCanvas');
+let gridColor = document.querySelector('#colorPicker').value //if nth is selected it should still be set black
 
 
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid(event) {
-    for (var row=1; row<=gridRow; row++) {
-        const newrow = document.createElement ('tr');
-        for (var column=1; column<=gridColumn; column++) {
-            const newcolumn = document.createElement('td');
-            const section= newrow.appendChild(newcolumn);
+    event.preventDefault();
+    
+    const gridRow = document.querySelector('#inputHeight').value;
+    const gridColumn = document.querySelector('#inputWidth').value;
+         
+    for (var i=0; i<gridRow; i++) {
+        const row = document.createElement ('tr');
+        for (var j=1; j<=gridColumn; j++) {
+            const column = document.createElement('td');
+            row.appendChild(column);
         }
-        newrow.appendChild(section);
+    var section = row;
+    const newtable=pixelcanvas.appendChild(section);
     }
-    const newtable = pixelcanvas.appendChild(pixelgrid);
-
+    document.querySelector('#submission').setAttribute('disabled', 'true');
 };
 
-
-function test (event){
-    console.log("test");
-};
 
 // Your code goes here!
-document.querySelector('#sizePicker').addEventListener('submit', test, event.preventDefault());
+document.querySelector('#sizePicker').addEventListener('submit', makeGrid);
+document.querySelector('#colorPicker').addEventListener('change', function() {
+    gridColor = document.querySelector('#colorPicker').value;
+});
+
+pixelcanvas.addEventListener('click', function(evt) {
+    if (evt.target.nodeName === 'TD') {
+        evt.target.style.backgroundColor=gridColor;
+    }
+});
 
