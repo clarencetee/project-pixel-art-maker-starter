c380c28eecd7a513b484843592723d0e0533e908
clarence tee <yoonsiangtee@gmail.com>
2020-04-23 16:05:29 +0100

    t

--- designs.js
+++ designs.js
@@ -1,16 +1,16 @@
 // Let's define some variables for us to manipulate later
-const gridColour = document.querySelector('#colorPicker');
-const gridRow = document.querySelector('#inputHeight');
-const gridHeight = document.querySelector('#inputWidth');
+const gridColour = document.querySelector('#colorPicker').value;
+const gridRow = document.querySelector('#inputHeight').value;
+const gridColumn = document.querySelector('#inputWidth').value;
 const pixelcanvas=document.querySelector('#pixelCanvas');
 
 
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
-    for (row=1; row<=gridRow; row++) {
+    for (var row=1; row<=gridRow; row++) {
         let newrow = document.createElement ('tr');
-        for (column=1; column<=gridHeight; column++) {
+        for (var column=1; column<=gridColumn; column++) {
             let newcolumn = document.createElement('td');
             let pixelgrid= newrow.appendChild(newcolumn);
         }
