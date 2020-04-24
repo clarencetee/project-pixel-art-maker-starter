47a9fab17f7cb28612a629c676a26159d658dd0f
clarence tee <yoonsiangtee@gmail.com>
2020-04-24 11:46:46 +0100

    Finished project(see this version ignore previous)

--- designs.js
+++ designs.js
@@ -6,33 +6,40 @@ let gridColor = document.querySelector('#colorPicker').value //if nth is selecte
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid(event) {
-    event.preventDefault();
-    
-    const gridRow = document.querySelector('#inputHeight').value;
-    const gridColumn = document.querySelector('#inputWidth').value;
-         
-    for (var i=0; i<gridRow; i++) {
-        const row = document.createElement ('tr');
-        for (var j=1; j<=gridColumn; j++) {
-            const column = document.createElement('td');
-            row.appendChild(column);
+    event.preventDefault(); //to supress page reloading which can be the default submit action
+
+    if (document.querySelectorAll('td').length ===0) { //this is to prevent 'submit' from stacking tables on tables
+        const gridRow = document.querySelector('#inputHeight').value;
+        const gridColumn = document.querySelector('#inputWidth').value;
+            
+        for (var i=0; i<gridRow; i++) {
+            const row = document.createElement ('tr');
+            for (var j=1; j<=gridColumn; j++) {
+                const column = document.createElement('td');
+                column.addEventListener('click', function (evt) {
+                    evt.target.style.backgroundColor=gridColor
+                });
+                row.appendChild(column);
+            }
+        const newtable=pixelcanvas.appendChild(row);
         }
-    var section = row;
-    const newtable=pixelcanvas.appendChild(section);
     }
-    document.querySelector('#submission').setAttribute('disabled', 'true');
+
+    else { //the alternative conditional checks to see if a cell is highlighted already
+        const tablecells = document.querySelectorAll('td');
+        for (let i=0; i<tablecells.length; i++) {
+                if (tablecells[i].style.backgroundColor) {
+                    tablecells[i].style.backgroundColor='rgb(255,255,255)';
+                }           
+            }
+        }
+    
 };
 
-
 // Your code goes here!
-document.querySelector('#sizePicker').addEventListener('submit', makeGrid);
 document.querySelector('#colorPicker').addEventListener('change', function() {
     gridColor = document.querySelector('#colorPicker').value;
 });
 
-pixelcanvas.addEventListener('click', function(evt) {
-    if (evt.target.nodeName === 'TD') {
-        evt.target.style.backgroundColor=gridColor;
-    }
-});
+document.querySelector('#sizePicker').addEventListener('submit', makeGrid);
 
